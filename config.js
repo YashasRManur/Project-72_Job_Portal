fa9065a4534671f9c933b99cd3b5cfcd1b865de6
YashasRManur <79317894+YashasRManur@users.noreply.github.com>
2021-10-29 19:48:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+require('@firebase/firestore');
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDwTvs7rxKaGVKtT_dANHq3fpehOBheBkI",
+  authDomain: "job-portal-27490.firebaseapp.com",
+  projectId: "job-portal-27490",
+  storageBucket: "job-portal-27490.appspot.com",
+  messagingSenderId: "370794309267",
+  appId: "1:370794309267:web:22fef414b75da1a8554f66"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default  firebase.firestore()
